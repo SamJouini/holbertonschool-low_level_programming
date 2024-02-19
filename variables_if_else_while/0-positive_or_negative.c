84ea5580949f0731b2d6888053b1c60410b7dcdb
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-19 14:18:48 +0000

    n is ...

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n>0)
-printf("is positive\n");
+	printf("is positive\n");
 
 else if (n==0)
-printf("is zero\n");
+	printf("is zero\n");
 
 else if (n<0)
-printf("is negative\n");
+	printf("is negative\n");
 
 return (0);
 }
