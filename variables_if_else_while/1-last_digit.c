25911ec194cfa2ec4bd741a8661d3835a876c0e3
Your Name <samantha.u.h.jouini@gmail.com>
2024-02-20 08:52:23 +0000

    is positiv

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,8 +19,8 @@ int main(void)
 			if (lastDigit > 5)
 				printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 			else if (lastDigit == 0)
-				printf("Last digit of %d is %d and 0\n", n, lastDigit);
+				printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 			else
-				printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastDigit);
+				printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 		return (0);
 }
