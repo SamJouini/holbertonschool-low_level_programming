ebb75af5bd6a4a383e1b2315b4e5a0e37479c574
Your Name <samantha.u.h.jouini@gmail.com>
2024-02-20 09:16:46 +0000

    betty is less than 6

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,20 +9,20 @@
 
 int main(void)
 {
-		int lastDigit;
-		int n;
-			srand(time(0));
-			n = rand() - RAND_MAX / 2;
+	int lastDigit;
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-			lastDigit = n % 10;
-			printf("Last digit of %d is %d and is ", n, LastDigit);
+	lastDigit = n % 10;
+	printf("Last digit of %d is %d and is ", n, lastDigit);
 
-			if (lastDigit > 5)
-				printf("greater than 5\n");
+	if (lastDigit > 5)
+		printf("greater than 5\n");
 
-			else if (lastDigit == 0)
-				printf("0\n", n, lastDigit);
-			else
-				printf("less than 6 and not 0\n", n, lastDigit);
-		return (0);
+	else if (lastDigit == 0)
+		printf("0\n");
+	else
+		printf("less than 6 and not 0\n");
+	return (0);
 }
