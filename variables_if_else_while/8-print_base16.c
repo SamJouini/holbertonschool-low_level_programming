32a9631b1765140e7502e89a8d2ce274a841edbc
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-21 11:30:52 +0000

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - number to ten
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
