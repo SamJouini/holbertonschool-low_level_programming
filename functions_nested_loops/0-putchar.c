b53b24000e6b4fcc9c0ba8a18b0c7e20e79ea444
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-21 14:44:16 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - printf
+ * main - p u t c h a r
  * Return: 0
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
