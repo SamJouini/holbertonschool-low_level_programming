baed5d7266b6304cd566c3d5e0ce065a4e04a498
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-21 09:53:57 +0000

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - number to ten
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+
+	}
+	putchar('\n');
+	return (0);
+}
