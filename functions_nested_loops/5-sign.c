29dae1c577c6ae45607d3f9e2b5df09ecb3025a5
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-22 14:04:15 +0000

    signs

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,21 +9,21 @@
 int print_sign(int n)
 
 {
-	if (n>0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
 
-	else if (n<0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
 
-	else 
+	else
 	{
-		_putchar('=');
+		_putchar('0');
 		return (0);
 	}
 }
