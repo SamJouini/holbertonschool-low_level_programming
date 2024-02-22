5e85b028f4aadb45a34e64bc70424e27f7c83a18
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-22 11:14:10 +0000

    alphabetx10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet x 10
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch, l;
+
+	for (ch = '0'; ch <'9'; ch++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+		_putchar('\n');
+	}
+}
+
