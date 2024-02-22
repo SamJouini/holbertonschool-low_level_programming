e93c4188d29511313d1bcf550643ea331d2e57a5
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-22 14:38:50 +0000

    absolute value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integrer
+ * @n: numbers
+ * Return: 0
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
