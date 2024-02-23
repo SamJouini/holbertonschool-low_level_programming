15b9e7816df147f1e7d51e4a5b1f15aa8577e04c
SamJouini <Samantha.u.h.jouini@gmail.com>
2024-02-23 14:46:32 +0000

    print the time table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - print the time table
+ *
+ * return:0
+ */
+
+void times_table(void)
+
+{
+	int a, b, r;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			r = a * b;
+
+			if (r <= 9)
+				_putchar(' ');
+			else
+				_putchar((r / 10) + '0');
+
+			_putchar((r / 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
