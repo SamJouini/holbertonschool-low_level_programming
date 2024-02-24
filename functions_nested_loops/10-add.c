91bd90e2785ca4f26c3f6d8c02860590c35c0edf
SamJouini <samantha.u.h.jouini@gmail.com>
2024-02-24 13:21:06 +0000

    addition of integrer

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adding two integers and return the value
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
