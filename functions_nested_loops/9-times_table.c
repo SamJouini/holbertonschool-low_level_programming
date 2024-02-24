79df301e383857370a0e92ba27daae8493e980a4
SamJouini <samantha.u.h.jouini@gmail.com>
2024-02-24 13:01:19 +0000

    time table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,26 +7,32 @@
  */
 
 void times_table(void)
-
 {
 	int a, b, r;
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar('0');
 
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar(',');
-			_putchar(' ');
 
 			r = a * b;
 
 			if (r <= 9)
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar((r / 10) + '0');
+			}
+			_putchar((r % 10) + '0');
 
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
