284960da6f95ecf380e95206b6b65bf4062ebd83
SamJouini <samantha.u.h.jouini@gmail.com>
2024-02-26 13:48:11 +0000

    print a line in the terminal

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print a line in the terminal
+ * @n : number
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (a = 1 ; a <= n ; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
