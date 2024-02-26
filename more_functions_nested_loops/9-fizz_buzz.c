5f474e22c3d9f88b657d7be38fed82c35e604db5
SamJouini <samantha.u.h.jouini@gmail.com>
2024-02-26 16:04:19 +0000

    fizz buzz exercice

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,21 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if	(n % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (n < 100)
 		{
 			printf("%d", n);
+			putchar(' ');
 		}
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
