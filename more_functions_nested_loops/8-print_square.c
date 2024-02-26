7cef67f4797f46a55fa7ff0713120897343638ba
SamJouini <samantha.u.h.jouini@gmail.com>
2024-02-26 14:49:05 +0000

    ascii art square with #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print a square of #
+ * @size: size of the square
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int l, h;
+
+	if (size < 1)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (l = 0; l < size; l++)
+	{
+		for (h = 0; h < size; h++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
