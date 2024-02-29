0c3c37f0712ae0abdcdcf640d5b690bb63bc4c28
SamJouini <samantha.u.h.jouini@gmail.com>
2024-02-29 10:10:36 +0000

    copy a pointed string into a buffer

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - print a pointed string
+ * @dest: buffer pointer
+ * @src : source
+ * Return: dest 
+ */
+
+char *_strcpy(char *dest, char *src)
+{                                                                                                                                                            
+	int i, length;
+
+	length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+
+}
