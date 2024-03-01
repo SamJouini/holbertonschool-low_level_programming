eb72dbbae87ae5356fd1cb66d0e392ff7a0787bb
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-01 11:58:30 +0000

    compares two strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: difference between strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0')  && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
