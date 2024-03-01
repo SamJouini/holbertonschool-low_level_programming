a9e3f853d4d2d9612bdd468fc9f7ae65cb21fc2c
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-01 11:29:40 +0000

    copying a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,17 +10,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-	while (*src && n > 0)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
-		*dest++ = *src++;
-		n--;
+		dest[i] = src[i];
 	}
 
-	while (n > 0)
+	for ( ; i < n; i++)
 	{
-		*dest++ = '\0';
-		n--;
+		dest[i] = '\0';
 	}
 
 	return (dest);
