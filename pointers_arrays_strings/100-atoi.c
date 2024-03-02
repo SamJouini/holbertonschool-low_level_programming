a6d6d6cc001f494006471d52492a74662db74f9f
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-02 11:22:07 +0000

    don't use array

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,25 @@
 
 int _atoi(char *str)
 {
-	int i;
 	int r = 0;
 	int positif = 1;
 	int start = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (; *str != '\0'; str++)
 	{
-		if (str[i] >= '0' && str[i] <= '9')
+		if (*str >= '0' && *str <= '9')
 		{
-			char c = str[i];
+			char c = *str;
 			int d = c - '0';
 
 			r = r * 10 + d;
 			start = 1;
 		}
-		else if (str[i] == '-')
+		else if (*str == '-')
 		{
 			positif = positif * -1;
 		}
-		else if (str[i] == '+')
+		else if (*str == '+')
 		{
 			/* nothing to do here just skip */
 		}
