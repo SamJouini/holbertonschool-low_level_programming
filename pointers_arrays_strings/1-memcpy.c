61387256abc7ccee578b254d500b625a59ef341a
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-04 15:30:56 +0100

    copying memory area from source to destination

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _memcpy - copy memory area
+ * _memcpy - copy memory area from source to destination
  * @n: number of byte to copy
  * @src: source to copy
  * @dest: destination of copied bytes
- * return: destination memory area
+ * Return: destination memory area
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (i < n) 
+	while (i < n)
 	{
 		dest[i] = src[i];
 		i++;
