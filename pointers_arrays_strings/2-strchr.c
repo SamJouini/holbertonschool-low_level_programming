2f4159a6e6b3dbd5609946570a33b05e40418c64
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-04 18:55:05 +0100

    find a caracter in a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,26 +3,23 @@
 
 /**
  * _strchr - location of a character in a string
- *
- *
- * Return:
+ * @c: character to locate
+ * @s: string
+ * Return: location of c
  */
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != c && s[i] != '\0')
+	while (s[i] != '\0')
 	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
 		i++;
 	}
 
-	if (s[i] == c)
-	{
-		return (&s[i]);
-	}
-	else
-	{
-		return (NULL);
-	}
+	return (NULL);
 }
