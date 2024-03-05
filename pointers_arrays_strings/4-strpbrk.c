937515f20fc6330a0ae4b88e47a8df30939e26ab
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-05 13:51:06 +0100

    compare two string to find the first common occurence

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searching a string for any of a set of bytes
+ * @s: the string to look trought
+ * @accept: the string to compare with
+ * Return: location of the first occurence
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
