8beab55c25b6c4b112f53d82083d2c620858a2e6
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-07 13:45:12 +0100

    multiplication of args

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 1;
 
-	    if (argc < 3)
-	    {
-		    printf("Error\n");
-		    return (1);
-	    }
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 	for (i = 1; i < argc; i++)
 	{
