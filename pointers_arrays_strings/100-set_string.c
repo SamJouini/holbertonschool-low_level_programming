3331a1c9871b3469d209665fd60e561d355074bd
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-07 13:58:58 +0100

    double pointer to pointed char

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string -  the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer to a char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
