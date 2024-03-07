d4e6a3125eed6216858e9fe735ee964a026fbb0c
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-07 13:37:33 +0100

    multiplication of args

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply args
+ * @argc: number in the command line
+ * @argv: array of argc's lenght
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 1,
+
+	    if (argc < 3)
+	    {
+		    printf("Error\n");
+		    return (1);
+	    }
+
+	for (i = 1; i < argc; i++)
+	{
+		sum *= atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
