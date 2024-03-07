42e7361635acfdfdfa389942e435fad59e626cf1
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-07 10:40:21 +0100

    is n a prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * is_prime_number_test - check if number is prime
+ * @n: number
+ * @base: number used as a test
+ * Return: 1 if prime or 0 is not
+ */
+
+int is_prime_number_test(int n, int base)
+{
+
+	if (base <= 1)
+	{
+		return (0);
+	}
+	if (n * n > base)
+	{
+		return (1);
+	}
+	if (base % n == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number_test(n + 1, base));
+}
+
+
+/**
+ * is_prime_number - check if a number is prime
+ * @n: number
+ * Return: 1 if prime or 0 if not
+ */
+
+int is_prime_number(int n)
+
+{
+	return (is_prime_number_test(2, n));
+}
