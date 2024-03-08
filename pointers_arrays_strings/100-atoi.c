9b158e1d449246b92c1855641f1452f0a4eb6e3e
SamJouini <148918489+SamJouini@users.noreply.github.com>
2024-03-08 15:59:28 +0100

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -22,10 +22,6 @@ int _atoi(char *str)
 			r = r * 10 + d;
 			start = 1;
 		}
-		if (r == INT_MAX / 10 && str++ > INT_MAX % 10)
-		{ 
-			return (INT_MIN);
-		}
 		else if (*str == '-')
 		{
 			positif = positif * -1;
