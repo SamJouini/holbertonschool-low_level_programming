3566b5098db6c7309a62b5f868f8d9da0f94036c
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-13 14:32:09 +0100

    updated exercices

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -36,7 +36,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 	{
-		return ("");
+		return (NULL);
 	}
 
 	str = malloc(_strlen(s1) + _strlen(s2) + 1);
--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * free_grid - frees a 2D grid previously created by alloc_grid function
