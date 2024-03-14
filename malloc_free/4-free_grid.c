916c273bedcd93cf9bbd93170dad78bccea21983
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-14 17:08:47 +0100

    somehow i missed whitespaces

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,17 +5,16 @@
  * free_grid - frees a 2D grid previously created by alloc_grid function
  * @grid: grind to free
  * @height: height to the array
- * Return: no return
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i < height; i++) 
+	for (i = 0; i < height; i++)
 	{
-		free(grid[i]); 
-	} 
-	
-	free(grid); 
+		free(grid[i]);
+	}
+
+	free(grid);
 }
