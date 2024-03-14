a88b68ee8f1c77cfc2363d28a26187e6419ddf74
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-14 12:01:40 +0100

    better print a struc dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -22,14 +22,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	}
 
-	if (d->age < 0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %f\n", d->age);
-	}
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 	{
