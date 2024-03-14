974ede50b8ce3170cc3edbfdacf253fc1bb3ff39
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-14 11:55:34 +0100

    print a struc dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print struct dog
+ * @d: dog's ID structure
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	if (d->age <= 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
