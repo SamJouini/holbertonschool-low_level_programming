d6a825581a1cfa817b3effa3b817065ab86fd696
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-14 19:07:52 +0100

    create a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,6 +13,11 @@ int _strlen(char *s)
 
 	i = 0;
 
+	if (s == NULL)
+	{
+		return (0);
+	}
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -36,6 +41,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
