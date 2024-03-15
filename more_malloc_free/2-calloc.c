5eb74e07f387489e2cd71c5c43dc09210c0b68b6
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-15 11:29:50 +0100

    calloc try 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -21,6 +21,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(nmemb * size);
 
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < (nmemb * size); i++)
 	{
 		ptr[i] = 0;
