9645220eaae99cafcaab72602c4b37b5441e65cc
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-15 11:20:19 +0100

    array-range 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,12 +10,14 @@
 int *array_range(int min, int max)
 {
 	int i;
-	int *arr = (int *)malloc(sizeof(int) * (max - min + 1));
+	int *arr;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
+
+	arr = (malloc(sizeof(int) * (max - min + 1)));
 
 	if (arr == NULL)
 	{
