a3004ea3c3cdc2cc25501a54978574227a40877b
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-19 11:16:09 +0100

    addition of n parameters

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - addition of n parameters
+ * @n: number of parameters
+ * Return: result of the addition
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list para;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(para, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(para, int);
+	}
+	va_end(para);
+	return (sum);
+}
