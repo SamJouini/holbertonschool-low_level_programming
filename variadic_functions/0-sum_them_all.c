99c4c4261a8101c5922b7f55aa5e309cf476a842
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-19 12:50:01 +0100

    addition of n parameters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,12 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
+	va_start(para, n);
+	
 	if (n == 0)
 	{
 		return (0);
 	}
-
-	va_start(para, n);
 
 	for (i = 0; i < n; i++)
 	{
