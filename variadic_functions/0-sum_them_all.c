dd98522b9737dbb963166dd999424278545ff90e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-20 09:19:47 +0100

    addition of parameters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
 
 /**
@@ -19,9 +18,10 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 
+	sum = 0;
 	va_start(para, n);
 
-	for (i = 0, sum = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(para, int);
 	}
