f0ff2e0232510ed73a308909c552e7458e7ed292
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-20 09:35:59 +0100

    trying const char

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(arg, char *));
+		printf("%s", va_arg(arg, const char *));
 
 		if (str != NULL)
 		{
