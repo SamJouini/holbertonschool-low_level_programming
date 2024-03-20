4eb93625604898d9f01e4f08a67a3cbced709741
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-20 10:40:42 +0100

    supress spaces

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,23 +12,17 @@ int sum_them_all(const unsigned int n, ...)
 	va_list para;
 	unsigned int i;
 	int sum;
-	int j;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-
 	sum = 0;
 	va_start(para, n);
-
 	for (i = 0; i < n; i++)
 	{
-		j = va_arg(para,int);
-		sum += j;
+		sum += va_arg(para, int);
 	}
-
 	va_end(para);
-
 	return (sum);
 }
