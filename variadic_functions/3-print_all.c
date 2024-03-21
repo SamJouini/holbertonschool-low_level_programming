a12e88e15dc128e3d79ba983296ca0eed7cc02f1
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-21 20:05:14 +0100

    testing the checker

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -60,12 +60,12 @@ void print_all(const char * const format, ...)
 	void (*f)(va_list);
 	char *separator = "";
 
-type_t format_list[] = {
-	{'c', format_char},
-	{'i', format_int},
-	{'f', format_float},
-	{'s', format_string}
-};
+	type_t format_list[] = {
+		{'c', format_char},
+		{'i', format_int},
+		{'f', format_float},
+		{'s', format_string}
+	};
 
 	va_start(arg_list, format);
 
