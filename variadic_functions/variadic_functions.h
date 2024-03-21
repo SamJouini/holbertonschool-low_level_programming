effedb6be7c961241429175ef89d1ca957ed4fb9
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-21 20:03:06 +0100

    updated header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,13 +1,18 @@
-#include <stdarg.h>
 #ifndef VARIADICFUNCTIONS_H
 #define VARIADICFUNCTIONS_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-struct type 
+/**
+ * struct type - used for the print all.c
+ * @type: type de format
+ * @f: function to call
+*/
+struct type
 {
 	char type;
 	void (*f)(va_list);
