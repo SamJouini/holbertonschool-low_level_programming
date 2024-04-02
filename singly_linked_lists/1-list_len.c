56173003f413a8a4d62e886b95912dc719d61f39
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-02 11:13:20 +0200

    forgot betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,6 +3,7 @@
 /**
  * list_len - function that returns the number of elements in a linked list
  * @h: head of the linked list
+ * Return: pointer to the head of the list
 */
 
 size_t list_len(const list_t *h)
