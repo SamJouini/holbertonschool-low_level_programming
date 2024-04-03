18aa907ded8e0c2379583719c06f8c43cb1d6caf
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-03 12:27:04 +0200

    correction

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,8 +2,9 @@
 
 /**
  * *get_dnodeint_at_index - the nth node of a double linked list
- * @head:
- * @index: 
+ * @head: head of the linked list
+ * @index: index of the node
+ * Return: the node or NULL if not existing
 */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
