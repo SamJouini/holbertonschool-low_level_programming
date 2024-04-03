a86b0392082d6467f31230f395ae4ad434e84c07
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-03 12:08:58 +0200

    correcting add

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,6 +23,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->next = *head;
 
+	if (*head == NULL)
+	{
+		*head->prev = NULL;
+	}
+
 	*head = new;
 	return (new);
 }
