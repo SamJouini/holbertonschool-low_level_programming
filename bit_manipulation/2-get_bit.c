04c789e0ad75cad40f50df3d640a2212c7870dcc
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-04 10:53:14 +0200

    grammar

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -2,8 +2,8 @@
 
 /**
  * get_bit - return the value of a bit at a given index
- * @n: the bit
- * @index: the index
+ * @n: the bit to 'mesure'
+ * @index: localisation of the bit to value
  * Return: the value of the bit at index or -1 if an failed
 */
 
