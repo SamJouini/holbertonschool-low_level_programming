36fe35bda5b0bf33739f98e65aca9da9870b8c12
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-05 13:57:53 +0200

    new testing

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -43,10 +43,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
 		close(file);
-		return (-1);
+		return (1);
 	}
 
 	bytes_written = write(file, text_content, _strlen(text_content));
